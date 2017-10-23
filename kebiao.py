1c7172b5175813136034d4b9cc802862c8c2a283
Ctios <1196234610@qq.com>
2017-10-23 14:49:33 +0800

    10.23

--- kebiao.py
+++ kebiao.py
@@ -10,7 +10,7 @@ course = {'cname': '',
           'week': '',
           'teacher': '',
           'classroom': ''}
-courses={}
+courses = {}
 course_data = {'num': '', 'courses': courses}
 time = {}
 week_data = {'long': '', 'time': time}
@@ -31,12 +31,14 @@ def init(num):
     f.write(js)
     f.close()
 
+
 def load_data():
-    f=open('data.json','r')
-    js=f.read()
-    data=json.loads(js)
-    print(data)
-    
+    f = open('data.json', 'r')
+    js = f.read()
+    data = json.loads(js)
+    return data
+
+
 def show_msg():
     print()
 
@@ -46,7 +48,16 @@ def showhelp():
 
 
 def init_homework():
-    course_name={}
+    data = load_data()
+    c_list=[]
+    courses = data['course_data']['courses']
+    for i in courses.keys():
+        c_list.append(courses[i]['cname'])
+    c_list=list(set(c_list))
+    for x in c_list:
+        
+        
+        
     print('init_homework')
 
 
@@ -68,29 +79,33 @@ def show_homework():
 
 
 def main(argv):
+    
     try:
         opts, args = getopt.getopt(argv, 'hi:s:', ['help', 'init=', 'show='])
     except getopt.GetoptError:
         error()
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            showhelp()
-        elif opt in ('-i', '--init'):
-            if arg.isdigit():
-                init(int(arg))
-            elif arg == 'homework':
-                init_homework()
-            else:
-                error()
-        elif opt in ('-s', '--show'):
-            if arg.lower() == 'today':
-                show_today()
-            elif arg.lower() == 'all':
-                show_all()
-            elif arg.lower() == 'homework':
-                show_homework()
-            else:
-                error()
+    if len(opts) > 1:
+        error()
+    opt = opts[0][0]
+    arg = opts[0][1]
+    if opt in ('-h', '--help'):
+        showhelp()
+    elif opt in ('-i', '--init'):
+        if arg.isdigit():
+            init(int(arg))
+        elif arg == 'homework':
+            init_homework()
+        else:
+            error()
+    elif opt in ('-s', '--show'):
+        if arg.lower() == 'today':
+            show_today()
+        elif arg.lower() == 'all':
+            show_all()
+        elif arg.lower() == 'homework':
+            show_homework()
+        else:
+            error()
 
 
 if __name__ == '__main__':
